ce4d82159692b26c428cfcb0940bb75a8ab352b5
rkodithy <156540341+rkodithy@users.noreply.github.com>
2024-03-11 19:28:39 -0700

    Update main.c

--- PIC/main.c
+++ PIC/main.c
@@ -1,1 +1,47 @@
+#include "mcc_generated_files/mcc.h"
+#include "mcc_generated_files/examples/i2c1_master_example.h"
 
+#include <stdint.h>
+#include <stdio.h>
+
+#define AS5600_I2C_ADDR 0x36
+#define AS5600_REG 0x0D
+
+
+void main(void)
+{
+    SYSTEM_Initialize();
+    EUSART2_Initialize();
+    I2C1_Initialize();
+
+    uint8_t data;
+
+    while (1)
+    {
+        i2c1_error_t result = I2C1_Open(AS5600_I2C_ADDR);
+        
+        __delay_ms(10);
+
+        if (result == I2C1_NOERR)
+        {
+            printf("Success! \n\r");
+            __delay_ms(100);
+            
+            data = I2C1_Read1ByteRegister(AS5600_I2C_ADDR, AS5600_REG);
+            
+            int angle = (float)data * 16 / 4096 * 360;
+        
+            printf("Data Value: %u\n\r", angle);
+        }
+        else if (result == I2C1_BUSY)
+        {
+            printf("I2C Busy");
+            __delay_ms(100);
+        }
+        else
+        {
+            printf("Error!");
+            __delay_ms(100);
+        }
+    }
+}
